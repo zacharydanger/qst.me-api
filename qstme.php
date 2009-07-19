557aaa9532cf499a0d3d8738304eb97b104a47fa
Zachary Danger <zacharydangercampbell@gmail.com>
2009-07-18 21:48:25 -0500

    Initial raw class.

---
+++ qstme.php
@@ -0,0 +1,36 @@
+<?php
+class Qstme {
+	public static $error = false;
+
+	public static function getUrl($url) {
+		self::$error = false;
+		$url = "http://qst.me/api.php?url=" . filter_var($url, FILTER_SANITIZE_URL);
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		$result = curl_exec($ch);
+		$info = curl_getinfo($ch);
+		self::_checkError($info);
+		curl_close($ch);
+		return $result;
+	}
+
+	private static function _checkError($info) {
+		if(200 !== intval($info['http_code'])) {
+			self::$error = true;
+		}
+	}
+}
+
+$Q = new Qstme();
+$result = $Q->getUrl("http://google.com/");
+if(true == $Q->error) {
+	echo "Error: ";
+}
+echo $result;
+echo "<hr />";
+$result = Qstme::getUrl("google.com/");
+if(true == Qstme::$error) {
+	echo "Error: ";
+}
+echo $result;
+?>
